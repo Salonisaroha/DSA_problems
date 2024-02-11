5cc9631cef49353823b342a0adb528d77a2d689e
Saloni Saroha <salonisaroha5@gmail.com>
2024-02-11 18:59:41 +0530

    problems of array

--- Arrays/problem1.cpp
+++ Arrays/problem1.cpp
@@ -1,5 +1,30 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+
+int secondlargest(int arr[], int n) {
+    int largest = 0, secondLargest = -1;
+ 
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[largest])
+            largest = i;
+    }
+ 
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != arr[largest]) {
+           
+            if (secondLargest == -1)
+                secondLargest = i;
+            else if (arr[i] > arr[secondLargest])
+                secondLargest = i;
+        }
+    }
+    return secondLargest;
+}
+ 
 int main(){
     //Question 1:- Find the total number of pairs in the Array whose sum is equal to the given value of x.
 
@@ -51,5 +76,60 @@ for(int i = 0; i<cap; i++){
         cout<<val[i]<<endl;
     }
 }
+
+// Question 4:- Find the second largest elements int the given array.
+
+int arr1[] = { 12, 35, 1, 10, 34, 1 };
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int second_Largest = secondlargest(arr1, n1);
+    if (second_Largest == -1)
+        cout << "Second largest didn't exit\n";
+    else
+        cout << "Second largest : " << arr1[second_Largest];
+
+// question 5 :-Rotate the given array a by k steps, where k is non-negative.
+int rotate[] ={1,2,3,4,5};
+int rotatesize = 5;
+int k = 2;
+k = k%rotatesize;
+int ansarray[5];
+int j = 0;
+for(int i = n-k; i<rotatesize; i++){
+    
+  ansarray[j++] = rotate[i]; 
+}
+
+for(int i = 0; i<=k; i++){
+    ansarray[j++] = rotate[i];
+}
+for(int i=0; i<rotatesize; i++){
+    cout<<ansarray[i];
+}
+
+// Question 6 :- Given q queries. check if the given number is present in the array or not.
+
+int noOfelement;
+cin>>noOfelement;
+vector<int> v(n);
+for(int i = 0; i<noOfelement; i++){
+    cin>>v[i];
+}
+
+const int N = 1e5 + 10;
+vector<int> freq(N,0);
+for(int i=0; i<n;i++){
+    freq[v[i]]++;
+}
+cout<<"enter queries :- ";
+int q;
+cin>>q;
+
+while(q--){
+    int querelement;
+    cin>>querelement;
+    cout<<freq[querelement]<<endl;
+}
+
+
 
 }
