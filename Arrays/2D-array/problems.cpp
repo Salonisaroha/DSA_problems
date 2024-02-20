1c7f09119a80821be95da79ac3972c183d1f1625
Saloni Saroha <salonisaroha5@gmail.com>
2024-02-20 22:33:43 +0530

    Matrix Multiplication and transpose'

--- Arrays/2D-array/problems.cpp
+++ Arrays/2D-array/problems.cpp
@@ -18,7 +18,42 @@ for(int i=0; i<n; i++){
 
 // Write a program to display multiplication of two matrices entered by the user
 
- 
+int r1,c1;
+cin>>r1>>c1;
+int A[r1][c1];
+for(int i = 0; i<r1; i++){
+    for(int j=0; j<c1; j++){
+       cin>>A[i][j];
+    }
+}
+int r2, c2;
+cin>>r2>>c2;
+int B[r2][c2];
+for(int i=0; i<r2; i++){
+    for(int j=0; j<c2; j++){
+        cin>>B[i][j];
+    }
+}
+if(c1!=r2){
+    cout<<"Matrix multiplication is not valid";
+}
+int C[r1][c2];
+for(int i=0; i<r1; i++){
+    for(int j=0; j<c2; j++){
+         int value = 0;
+         for(int k=0; k<r2; k++){
+           value += A[i][k]*B[k][j];
+         }
+         C[i][j] = value;
+    }
+}
+for(int i = 0; i<r1; i++){
+    for(int j = 0; j<c2; j++){
+        cout<<C[i][j]<<" ";
+
+    }
+    cout<<endl;
+}
 
 // Question 2 :- Write a program to find the transpose of a matrix
 
