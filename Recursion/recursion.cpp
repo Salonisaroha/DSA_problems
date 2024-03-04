53936c0e529359854209dc8627c1399eb6095d35
Saloni Saroha <salonisaroha5@gmail.com>
2024-03-04 20:37:46 +0530

    Recursion in depth

--- Recursion/recursion.cpp
+++ Recursion/recursion.cpp
@@ -75,7 +75,56 @@ int countDistinctWyToClimbStair(long long nStairs){
     }
     return countDistinctWyToClimbStair(nStairs-1) + countDistinctWyToClimbStair(nStairs-2);
 }
-int main(){
+void reverse(string& str, int i, int j){
+    if(i>j){
+        return;
+    }
+    swap(str[i], str[j]);
+
+    i++;
+    j--;
+    reverse(str,i,j );
+}
+
+bool checkPalindrome(string str, int i, int j){
+ if(i>j){
+    return true;
+ }
+ if(str[i]!= str[j]){
+    return false;
+ }
+ else{
+    return checkPalindrome(str, i + 1, j -1);
+ }
+
+}
+int power(int a, int b){
+    if(b==0){
+        return 1;
+    }
+    if(b==1){
+        return a;
+    }
+    int ans = power(a, b/2);
+    if(b%2==0){
+        return ans*ans;
+    }
+    else{
+        return a*ans*ans;
+    }
+}
+void sortArray(int *arr, int n){
+    if(n==0 || n==1){
+         return;
+    }
+    for(int i = 0; i<n; i++){
+        if(arr[i]>arr[i+1]){
+            swap(arr[i], arr[i+1]);
+        }
+    }
+    sortArray(arr, n-1);
+}
+ int main(){
     int n;
     cin>>n;
     int ans = fact(n);
@@ -90,7 +139,7 @@ int main(){
    
         cout<<fibonacci(x);
     
-    // Question:- count ways to reach the n-th stairs.
+//     // Question:- count ways to reach the n-th stairs.
 
     int y;
     cin>>y;
@@ -147,6 +196,45 @@ if(res){
 else{
     cout<<"Element is not found !"<<endl;
 }
-    
-    return 0;
+
+// Reverse string
+
+string str = "Saloni Saroha";
+reverse(str, 0, str.length() - 1);
+cout<<str<<endl;
+
+// Question :- String is palindrome or not ? e.g. abbccbba
+
+string name = "abbccbba";
+cout<<endl;
+bool isPalindrome = checkPalindrome(name, 0, name.length()-1);
+if(isPalindrome){
+   cout<<"It is is Palindrome" <<endl;
+}
+else{
+    cout<<"It is not palindrome"<<endl;
+}
+
+// Question find the power.
+
+int a,b;
+cin>>a>>b;
+int ans = power(a,b);
+cout<<ans;
+
+//Bubble sort :- 
+int n;
+cin>>n;
+int arr[n];
+for(int i=0; i<n; i++){
+    cin>>arr[i];
+}
+ sortArray(arr, n);
+ for(int i = 0; i<n; i++){
+    cout<<arr[i];
+ }
+
+return 0;
+
+   
 }
