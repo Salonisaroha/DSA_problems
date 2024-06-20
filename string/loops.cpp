6649b216ea5ac18ab976d66289d28b02d67da7c3
Saloni Saroha <salonisaroha5@gmail.com>
2024-06-20 12:50:39 +0530

    adding changes

---
+++ string/loops.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ int main (){
+    string str = "Hello from apna college!";
+    for(int i=0; i<str.length(); i++){
+        cout<<str[i]<<" ";
+    }
+    cout<<endl;
+    // string str1 = "Coding is superlative degree!";
+    // for(char ch : str1){
+    //     cout<<str1<<" ";
+    // }
+
+    // Member functions
+
+    cout<<str.length()<<endl;
+    cout<<str.at(3)<<endl;
+    cout<<str.substr(1,10);
+    cout<<str.find("apna");
+ }
--- string/string.cpp
+++ string/string.cpp
@@ -7,5 +7,14 @@ int main() {
     char arr[5] = {'a', 'b', 'c', 'd', '\0'};
     cout << arr << endl;        // Print the array, which is null-terminated
     cout << strlen(arr) << endl; // Print the length of the string
+
+    string str = "hello world!";
+    cout<<str<<endl;
+
+    //taking input in the form of string with whitespaces;
+
+    string str1;
+    getline(cin,str1);
+    cout<<str1;
     return 0;
 }
