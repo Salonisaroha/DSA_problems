be2a6beaeec97c441a4df223dc8f3b3fceb026dd
Saloni Saroha <salonisaroha5@gmail.com>
2024-07-04 21:31:27 +0530

    Bitwise operator

---
+++ Bitwise operators/operator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // & operator
+    cout<<(2&5)<<endl;
+    // | operator
+
+    cout<<(10|90)<<endl;
+    // ^ operator
+
+    cout<<(2^4)<<endl;
+
+    // not operator
+    cout<<(~9);
+
+    // Left shift
+
+    cout<<(7<<2)<<endl; // formula for left shift is (a<<b) => a*2^b
+
+    // right shift
+    cout<<(7>>2)<<endl;
+
+
+}
