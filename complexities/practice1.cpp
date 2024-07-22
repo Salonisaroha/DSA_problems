517bce2b62362548d7a64859e39863202ffefa6b
Saloni Saroha <salonisaroha5@gmail.com>
2024-07-22 21:10:57 +0530

    adding changes

---
+++ complexities/practice1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            cout<<i<<""<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
