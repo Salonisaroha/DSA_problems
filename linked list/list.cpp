0c24f4a5a68a2ca881635ecbd088992fcb26c426
Saloni Saroha <salonisaroha5@gmail.com>
2024-08-16 17:47:04 +0530

    linked list operations

--- linked list/list.cpp
+++ linked list/list.cpp
@@ -28,7 +28,26 @@ public:
             head = newNode;
         }
     }
+    void push_back(int val){
+        Node* newNode = new Node(val);
+        if(head==NULL){
+            head=tail=newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    void printList(){
+        Node* temp = head;
+        while(temp!= NULL){
+            cout<<temp->data << "->";
+            temp = temp->next;
+        }
+        cout<<"NULL\n";
+    }
 };
+
 
 // Four main functions in linked list are:-
 // push front, push back, pop front, pop back   
@@ -38,6 +57,9 @@ int main(){
     ll.push_front(2);
     ll.push_front(1);
     ll.push_front(0);
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.printList();
 
     return 0;
 }
