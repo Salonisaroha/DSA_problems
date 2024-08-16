8c381270803699a487ace8bb7c5946ffce93d087
Saloni Saroha <salonisaroha5@gmail.com>
2024-08-16 19:47:06 +0530

    adding some changes

--- linked list/search.cpp
+++ linked list/search.cpp
@@ -1,7 +1,64 @@
 #include<iostream>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+class List{
+    Node* head;
+    Node* tail;
+    public:
+    List(){
+        head = NULL;
+        tail = NULL;
+    }
+    void printList(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+    void push_front(int val){
+         Node* newNode = new Node(val); // dynamic storage of new node.
+        if(head==NULL){
+            head = tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+int searchItr(int key){
+    Node* temp = head;
+    int idx = 0;
+    while(temp != NULL){
+        if(temp->data==key){
+            return idx;
 
+        }
+        temp = temp->next;
+        idx++;
+    }
+    return -1;
+}  
+};
 int main(){
+List ll;
+ll.push_front(4);
+ll.push_front(3);
+ll.push_front(2);
+ll.push_front(1);
+// ll.push_front(0);
+ll.printList();
+cout<<ll.searchItr(3)<<endl;
+cout<<ll.searchItr(40);
 
 }
