15682e287d74cffd48733ecd7109097230999052
Saloni Saroha <salonisaroha5@gmail.com>
2024-08-16 18:57:30 +0530

    adding commit changes

--- linked list/insertion.cpp
+++ linked list/insertion.cpp
@@ -3,36 +3,39 @@ using namespace std;
 
 class Node{
 public:
-int data;
-Node* next;
-Node(int val){
-   data=val;
-   next=NULL; 
-}
-~Node(){
-    if(next!=NULL){
-        delete next;
-        next= NULL;
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL; 
     }
-}
+    ~Node(){
+        if(next != NULL){
+            delete next;
+            next = NULL;
+        }
+    }
 };
+
 class List{
-Node* head;
-  Node* tail;
+    Node* head;
+    Node* tail;
 public:
     List(){
         head = NULL;
         tail = NULL;
     }
-   ~List(){
-    if(head!=NULL){
-        delete head;
-        head = NULL;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    
+    ~List(){
+        if(head != NULL){
+            delete head;
+            head = NULL; 
+        } 
     }
-   } 
-     void push_front(int val){
+
+    void push_front(int val){
         Node* newNode = new Node(val); // dynamic storage of new node.
-        if(head==NULL){
+        if(head == NULL){
             head = tail = newNode;
         }
         else{
@@ -40,58 +43,82 @@ public:
             head = newNode;
         }
     }
+    
     void insert(int val, int pos){
+        if(pos == 0){
+            push_front(val);
+            return;
+        }
         Node* newNode = new Node(val);
         Node* temp = head;
-        for(int i=0; i<pos-1; i++){
-            if(temp==NULL){
-                cout<<"position is invalid\n";
+        for(int i = 0; i < pos - 1; i++){
+            if(temp == NULL){
+                cout << "Position is invalid\n";
+                delete newNode;
                 return;
             }
             temp = temp->next;
         }
+        if(temp == NULL){
+            cout << "Position is invalid\n";
+            delete newNode;
+            return;
+        }
         newNode->next = temp->next;
-        temp->next= newNode;
+        temp->next = newNode;
+        if(newNode->next == NULL) {
+            tail = newNode;
+        }
     }
     
     void printList(){
         Node* temp = head;
-        while(temp!= NULL){
-            cout<<temp->data << "->";
+        while(temp != NULL){
+            cout << temp->data << "->";
             temp = temp->next;
         }
-        cout<<"NULL\n";
+        cout << "NULL\n";
     }
 
     void pop_front(){
-        if(head==NULL){
-            cout<<"ll is empty\n";
+        if(head == NULL){
+            cout << "Linked list is empty\n";
             return;
         }
-        Node* temp =head;
+        Node* temp = head;
         head = head->next;
         temp->next = NULL;
         delete temp;
     }
+
     void pop_back(){
+        if(head == NULL){
+            cout << "Linked list is empty\n";
+            return;
+        }
+        if(head->next == NULL){
+            delete head;
+            head = tail = NULL;
+            return;
+        }
         Node* temp = head;
         while(temp->next->next != NULL){
-                temp = temp->next;
+            temp = temp->next;
         }
-        temp->next = NULL;
         delete tail;
         tail = temp;
+        tail->next = NULL;
     }
 };
+
 int main(){
- List ll;  // Correct way to create an object of the List class
+    List ll;  
     ll.push_front(3);
     ll.push_front(2);
     ll.push_front(1);
     ll.push_front(0);
     ll.insert(100, 2);
     ll.pop_front();
-    ll.pop_back();
     ll.printList();
     return 0;
 }
