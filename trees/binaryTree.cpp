965208ac4e5a26cd921a450e10f34bf5823518d9
Saloni Saroha <salonisaroha5@gmail.com>
2024-09-15 16:09:10 +0530

    adding binary tree

--- trees/binaryTree.cpp
+++ trees/binaryTree.cpp
@@ -1,115 +1,129 @@
-// [1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1]
-
 #include<iostream>
 #include<vector>
 #include<queue>
 
 using namespace std;
-class Node{
+
+class Node {
 public:
-int data;
-Node* left;
-Node* right;
-Node(int data){
-    this->data = data;
-    left=right=NULL;
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
     }
 };
+
 static int idx = -1;
 
-Node* buildTree(vector<int> nodes){
-idx++;
-if(nodes[idx]== -1){
-    return NULL;
+Node* buildTree(vector<int> nodes) {
+    idx++;
+    if (nodes[idx] == -1) {
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
 }
-nodes[idx];
-Node* currNode = new Node(nodes[idx]);
 
-currNode->left = buildTree(nodes);
-currNode->right = buildTree(nodes);
-
-return currNode;
-}
-
-void preOrder(Node* root){
-
-    if(root==NULL){
+// preorder traversal
+void preOrder(Node* root) {
+    if (root == NULL) {
         return;
     }
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     preOrder(root->left);
     preOrder(root->right);
 }
 
-void inOrder(Node* root){
-    if(root==NULL){
+// inorder traversal
+void inOrder(Node* root) {
+    if (root == NULL) {
         return;
     }
-
     inOrder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     inOrder(root->right);
 }
 
-void postOrder(Node* root){
-    if(root==NULL){
+// postorder traversal
+void postOrder(Node* root) {
+    if (root == NULL) {
         return;
     }
     postOrder(root->left);
     postOrder(root->right);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
 }
 
-void levelOrder(Node* root){
-    if(root==NULL){
+// level order traversal
+void levelOrder(Node* root) {
+    if (root == NULL) {
         return;
     }
-    queue<Node*>q;
+    queue<Node*> q;
     q.push(root);
     q.push(NULL);
-    while(!q.empty()){
+
+    while (!q.empty()) {
         Node* curr = q.front();
         q.pop();
-        if(curr==NULL){
-            cout<<endl;
-            if(q.empty()){
+
+        if (curr == NULL) {
+            cout << endl;
+            if (q.empty()) {
                 break;
             }
             q.push(NULL);
+        } else {
+            cout << curr->data << " ";
+            if (curr->left != NULL) {
+                q.push(curr->left);
+            }
+            if (curr->right != NULL) {
+                q.push(curr->right);
+            }
         }
-        else{
-         cout<<curr->data<<" ";
-        if(curr->left !=NULL){
-            q.push(curr->left);
-
-        }
-        if(curr->right!= NULL){
-            q.push(curr->right);
-        }
-        }
-       
     }
-
 }
 
-int main(){
-    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+// height of binary tree
+int height(Node* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int leftHt = height(root->left);
+    int rightHt = height(root->right);
+
+    int currHt = max(leftHt, rightHt) + 1;
+    return currHt;
+}
+
+int main() {
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
     Node* root = buildTree(nodes);
-    cout<<"root "<< root->data <<endl;
+    cout << "Root: " << root->data << endl;
 
     preOrder(root);
-    cout<<endl;
+    cout << endl;
 
-inOrder(root);
-cout<<endl;
-    
+    inOrder(root);
+    cout << endl;
 
     postOrder(root);
-    cout<<endl;
+    cout << endl;
 
     levelOrder(root);
-    cout<<endl;
+    cout << endl;
+
+    cout << "Height: " << height(root) << endl;
 
     return 0;
 }
