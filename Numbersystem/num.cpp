389c57c3a8155921867dde480f5bf95b2b490a67
Saloni Saroha <salonisaroha5@gmail.com>
2024-09-30 21:24:18 +0530

    some changes done

--- Numbersystem/num.cpp
+++ Numbersystem/num.cpp
@@ -14,11 +14,28 @@ int decTobin(int decNum){
    
 
 }
+int binTodec(int binNum){
+    int ans = 0;
+    int pow = 1;
+    while(binNum>0){
+        int rem = binNum%10;
+        ans += (rem*pow);
+        binNum /= 10;
+         
+         pow *= 2;
+    }
+    return ans;
+   
+
+}
 
 int main(){
 int decNum = 50;
 for(int i=0; i<=10; i++){
    cout<<decTobin(i)<<endl;
+}
+for(int i=0; i<=10; i++){
+   cout<<binTodec(i)<<endl;
 }
 
 return 0;
