d52203686088afa315b42831fb4905f7e85a77c6
Saloni Saroha <salonisaroha5@gmail.com>
2024-10-12 11:40:21 +0530

    adding changes

---
+++ QuestionsOfArrays/aggressiveCow.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool isPossible(vector<int> &arr, int N, int C, int minAllowedDist){
+int cows = 1, lastStallPos = arr[0];
+for(int i =1; i<N; i++){
+    if(arr[i]-lastStallPos>= minAllowedDist){
+        cows++;
+        lastStallPos = arr[i];
+    }
+    if(cows==C){
+        return true;
+    }
+}
+return false;
+}
+int getDistance(vector<int> &arr, int N, int C){
+
+    sort(arr.begin(), arr.end());
+    int st = 1, end = arr[N-1] - arr[0], ans = -1;
+    while(st<=end){
+        int mid = st + (end-st)/2;
+        if(isPossible(arr, N, C, mid)){
+         ans = mid;
+         st = mid+1;
+        } else{
+            mid - 1;
+        }
+    }
+    return ans;
+}
+int main(){
+    int N = 5, C=3;
+    vector<int> arr = {1,2,8,4,9};
+    cout<<getDistance(arr, N,C)<<endl;
+    return 0;
+}
